e7684376ab76fea6bfde2263b5b7fa5581431e1f
strdr4605 <str.dr4605@gmail.com>
2018-05-05 22:52:10 +0200

    create simple multiplayer

---
+++ Assets/PlayerObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+    float speed = 3.0f;
+    public GameObject PlayerUnitPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        if( isLocalPlayer == false ) {
+            // This object belongs to another player.
+            return;
+        }
+
+        // Since the PlayerObject is invisible and not part of the world, give me something physical to move around!
+        Debug.Log("PlayerObject::Start");
+        CmdSpawnPlayer();
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if( isLocalPlayer == false) {
+            return;
+        }
+
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        CmdMoveUnit(move);
+	}
+
+    GameObject myPlayerUnit;
+
+    [Command]
+    void CmdSpawnPlayer() {
+        // Exec on the server.
+        GameObject go = Instantiate(PlayerUnitPrefab);
+        myPlayerUnit = go;
+        NetworkServer.Spawn(go);
+    }
+
+    [Command]
+    void CmdMoveUnit(Vector3 move) {
+        if(myPlayerUnit == null) {
+            return;
+        }
+
+
+        myPlayerUnit.transform.position += move * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/PlayerUnit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerUnit : NetworkBehaviour {
+    //float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+    void Update()
+    {
+        //if( isLocalPlayer == false ) {
+        //    return;
+        //}
+
+        //var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        //transform.position += move * speed * Time.deltaTime;
+    }
+}
