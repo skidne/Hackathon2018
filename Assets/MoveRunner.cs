b12ae4a9d29f41d292e589a5b8f0d3faf7b369e2
strdr4605 <str.dr4605@gmail.com>
2018-05-05 21:01:06 +0200

    change player movement

--- Assets/MoveRunner.cs
+++ Assets/MoveRunner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveRunner : MonoBehaviour
 {
+    float speed = 3.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,30 +12,10 @@ public class MoveRunner : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.W))
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(0, 3);
-			transform.eulerAngles = Vector3.up;
-		}
-		else if (Input.GetKey(KeyCode.A))
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-3, 0);
-			transform.eulerAngles = Vector3.left;
-		}
-		else if (Input.GetKey(KeyCode.S))
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(0, -3);
-			transform.eulerAngles = Vector3.down;
-		} 
-		else if (Input.GetKey(KeyCode.D))
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(3, 0);
-			transform.eulerAngles = Vector3.right;
-		}
-		else
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-		}
-	}
+
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
 }
